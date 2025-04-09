9197e261575ccae814d169fb5e85a6c38d88b2d6
JessicaHoang <jessica97hoang@gmail.com>
2025-04-09 13:59:14 -0700

    updated pokemon columns to their number and added hover motion

--- components/PokemonCard.tsx
+++ components/PokemonCard.tsx
@@ -1,5 +1,5 @@
-import {useState} from "react";
-import {useAnimation} from "framer-motion";
+import {useState, useEffect} from "react";
+import {useAnimation, motion} from "framer-motion";
 
 export interface Pokemon {
     url: string;
@@ -23,10 +23,62 @@ interface Props {
     pokemon: Pokemon;
 }
 const PokemonCard = ({pokemon}) => {
-    const[data, setData] = useState(null);
+    const[data, setData] = useState<PokemonData | null>(null);
     const[isHovered, setIsHovered] = useState(false);
     const controls = useAnimation();
-    return <div>PokemonCard</div>;
+
+    useEffect(() => {
+        const fetchPokemonData = async () => {
+            try {
+                const response = await fetch(pokemon.url);
+                if (!response.ok){
+                    throw new Error("failed to fetch")
+                }
+                const fetchedData: PokemonData = await response.json();
+                setData(fetchedData);
+            } catch(error) {
+                console.log(error)
+                return null
+            }
+        };
+
+        fetchPokemonData();
+    }, [pokemon.url]);
+
+    const getPokemonNumberFromUrl = (
+        url: string
+    ): string | null => {
+        const matches = url.match(/\/(\d+)\/$/);
+        return matches ? matches[1] : null;
+    };
+
+    const pokemonNumber = getPokemonNumberFromUrl(pokemon.url);
+    
+    return (
+        <div className="flex items-center justify center flex-col relative">
+            <motion.div 
+            className="cursor-pointer" 
+            animate={controls}
+            whileHover={{scale:0.5}}
+            whileTap={{scale: 0.9}}
+            >
+                <motion.div 
+                className="bg-gray-700 flex items-center round-md p-1 relative"
+                initial={{rotateY: 0}}
+                variants={{
+                    front: {rotateY: 0},
+                    back: {rotateY: 180}
+                }}
+                >
+                    <div 
+                    className="absolute top-0 text-white font-bold p-3 text-5xl">
+                        {pokemonNumber}
+                    </div>
+
+                </motion.div>
+            </motion.div>
+        </div>
+    )
 };
 
 export default PokemonCard;
