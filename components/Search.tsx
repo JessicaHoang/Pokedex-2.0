9ac576005fd0bad643d3251b9ee4ab4843597ffc
JessicaHoang <jessica97hoang@gmail.com>
2025-04-09 16:26:30 -0700

    textbox is white for more contrast and easy to read text

--- components/Search.tsx
+++ components/Search.tsx
@@ -32,7 +32,7 @@ const Search = ({search}: {search?:string}) => {
         className="block w-4/5 rounded-md border-0 py-1.5 px-1
          text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300
           placeholder:text-gray-400 focus:fing-2 focus:outline-yellow-500 
-          sm:text-sm sm:leading-6 mb-10"
+          sm:text-sm sm:leading-6 mb-10 bg-white"
         />
     </div>
 };
